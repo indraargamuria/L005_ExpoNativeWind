78822b3f9413e0e307b2b63ab19c151b9b18d32a
Indra Arga Muria <indraargaaa@gmail.com>
2025-04-13 12:25:49 +0700

    Add tab with gesture on top screen

--- components/Chat.tsx
+++ components/Chat.tsx
@@ -7,7 +7,7 @@ const Chat = () => {
   return (
     <SafeAreaView className="flex-1 bg-gray-50 p-4">
         <View className="flex-1 items-center justify-center bg-white dark:bg-gray-900">
-            <Text className="text-lg text-gray-900 dark:text-white font-bold">🏠 Home Page</Text>
+            <Text className="text-lg text-gray-900 dark:text-white font-bold">🏠 Home Pages</Text>
             <Text className="text-gray-600 dark:text-gray-300">Welcome to the homepage!</Text>
         </View>
     </SafeAreaView>
